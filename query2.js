111b4b7dfb34e7b7761d607660212ec72b25ec0d
jihan <jihanramos4@gmail.com>
2026-09-23 07:53:24 +0800

    remove scratch test files

--- query2.js
+++
@@ -1,1 +0,0 @@
-const db = require('./backend/db'); db.all(\SELECT * FROM tickets WHERE status = 'active'\, (err, rows) => { console.log(rows); });
--- test2.js
+++
@@ -1,8 +0,0 @@
-const db = require('./backend/db');
-db.all(
-  "SELECT * FROM slots WHERE status = 'occupied'",
-  (err, rows) => {
-    console.log("Occupied slots:", rows);
-  }
-);
-
